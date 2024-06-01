a5f7e07d9c0d13fd943fb70aa27c6e36f9f0a175
rievanzzz <arievan920@gmail.com>
2024-06-01 22:14:40 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::create([
+            'name' => 'Airevan',
+            'email' => 'ivan@mail.com',
+            'password' => bcrypt("password")
+        ]);
     }
 }
